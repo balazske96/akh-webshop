eab95da05612c6d388c392a66f8f50fb86b73cd2
Balázs <venyigebalazs96@gmail.com>
2020-12-11 19:07:42 +0100

    files re added

--- backend/AKHWebshop/AKHWebshop/Controllers/ProductController.cs
+++ backend/AKHWebshop/AKHWebshop/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using AKHWebshop.Models.Shop;
+using AKHWebshop.Models.Shop.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -9,17 +12,20 @@ namespace AKHWebshop.Controllers
     public class ProductController : ControllerBase
     {
         private readonly ILogger<Product> _logger;
+        private ShopDataContext _dataContext;
 
-        public ProductController(ILogger<Product> logger)
+        public ProductController(ILogger<Product> logger, ShopDataContext dataContext)
         {
             _logger = logger;
+            _dataContext = dataContext;
         }
 
         [HttpGet]
         public JsonResult GetProducts()
         {
-            Product product = new Product() {Name = "valami"};
-            return new JsonResult(product);
+            return new JsonResult(
+                _dataContext.Products.ToList()
+            );
         }
     }
 }
